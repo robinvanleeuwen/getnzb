0008b903f5c5aaf505f61d2cc2c05aadd0e99d38
robinvanleeuwen <robinvanleeuwen@a25b6c93-3019-6843-0890-3180a169f806>
2010-08-13 10:14:23 +0000

    Added GPLv3 licence. Added Licencing and (C)-comments.

--- getnzb/src/com/rvl/android/getnzb/activity/preferences.java
+++ getnzb/src/com/rvl/android/getnzb/activity/preferences.java
@@ -1,3 +1,28 @@
+
+/**
+ * This file is a part of GetNZB
+ * 
+ * GetNZB - http://code.google.com/p/getnzb
+ * "Android NZB Search and HellaNZB client."
+ * 
+ * Copyright (C) 2010: Robin van Leeuwen (robinvanleeuwen@gmail.com)
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+**/
+
 package com.rvl.android.getnzb.activity;
 
 import com.rvl.android.getnzb.tags;
